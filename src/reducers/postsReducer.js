6f9dda70dabd8c78472062bac81a28437e2496dc
Darryl <darrylmangibin@gmail.com>
2019-06-04 16:16:03 +0800

    reducers updated

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,4 @@
+import { POSTS_FAIL, POSTS_REQUEST, POSTS_SUCCESS } from '../actions/types';
 
 const initialState = {
   loading: false,
@@ -7,6 +8,23 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch(action.type) {
+    case POSTS_REQUEST:
+      return {
+        ...state,
+        loading: action.payload
+      }
+    case POSTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        posts: [...action.payload]
+      }
+    case POSTS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      }
     default:
       return state
   }
