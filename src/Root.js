cb57617b99d815445743ab9d12b3126d396887c8
Darryl <darrylmangibin@gmail.com>
2019-06-04 16:02:46 +0800

    store connected to the provider

--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+
+const store = configureStore()
 
 const Root = (props) => {
   return (
-    <Provider>
+    <Provider store={store}>
       {props.children}
     </Provider>
   )
