a85be897d1be53400ff7af5f12923899b4192c06
Darryl <darrylmangibin@gmail.com>
2019-06-04 16:32:11 +0800

    displaying all post loading and errors

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,12 +8,35 @@ class Post extends React.Component {
     this.props.postsRequest()
   }
 
+  renderPostList = (posts) => {
+    return posts.map(post => {
+      return (
+        <PostList 
+          key={post.id}
+          title={post.title}
+        />
+      )
+    })
+  }
+
+  renderErrorLoadingTitle = (error, loading) => {
+    if(error) {
+      return <h4>{error}</h4>
+    } else if(loading) {
+      return <h5>Loading...</h5>
+    }
+    return <h1>Post List</h1>
+  }
+
   render() {
+    const { posts, error, loading } = this.props.posts
     console.log(this.props)
     return (
       <div>
-        <h1>POST</h1>
-        <PostList />
+        {this.renderErrorLoadingTitle(error, loading)}
+        <ul>
+          {this.renderPostList(posts)}
+        </ul>
       </div>
     )
   }
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const PostList = () => {
+const PostList = (props) => {
   return (
-    <div>
-      PostList
-    </div>
+    <li>
+      {props.title}
+    </li>
   )
 }
 
